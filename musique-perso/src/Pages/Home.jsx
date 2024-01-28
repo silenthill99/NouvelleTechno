2334e7d1104206b4e9c0b2b50ae9b6401d0e80cc
silenthill99 <florian.graziani@sfr.fr>
2024-01-28 14:35:53 +0100

    Ajout du CSS des boutons

--- musique-perso/src/Pages/Home.jsx
+++ musique-perso/src/Pages/Home.jsx
@@ -1,5 +1,7 @@
 import { CiPlay1 } from "react-icons/ci";
 import { CiPause1 } from "react-icons/ci";
+import { CiStop1 } from "react-icons/ci";
+import "../css/Home.css"
 
 export const Home = () => {
     return (
@@ -10,9 +12,10 @@ export const Home = () => {
                 <span id="elapsed">0:00</span> / <span id="track-time">1:00</span>
             </div>
             <div>
-                <audio src="KANNA movie version.mp3" controls/>
-                <CiPlay1 />
-                <CiPause1 />
+                <audio src={"KANNA movie version.mp3"}/><br/>
+                <CiPlay1 className="button" id="play-button"/>
+                <CiPause1 className="button" id="pause-button"/>
+                <CiStop1 className="button" id="stop-button"/>
             </div>
         </main>
     )
